b618744978409dd74d21c7ac9dc1c7d30da33c54
Thomas Carlier <carlier.thomas@gmail.com>
2021-09-21 21:22:17 +1000

    Add selectors

--- src/app/shared/state/books.reducer.ts
+++ src/app/shared/state/books.reducer.ts
@@ -16,7 +16,7 @@ export interface State {
   collection: BookModel[];
   selectedBookId: string | null;
 }
- 
+
 export const initialState: State = {
   collection: [],
   selectedBookId: null,
@@ -35,9 +35,49 @@ export const booksReducer = createReducer(
       ...state,
       selectedBookId: action.bookId,
     };
+  }),
+  on(BooksApiActions.booksLoaded, (state, action) => {
+    return {
+      ...state,
+      collection: action.books,
+    };
+  }),
+  on(BooksApiActions.bookCreated, (state, action) => {
+    return {
+      ...state,
+      collection: createBook(state.collection, action.book),
+    };
+  }),
+  on(BooksApiActions.bookUpdated, (state, action) => {
+    return {
+      ...state,
+      collection: updateBook(state.collection, action.book),
+    };
+  }),
+  on(BooksApiActions.bookDeleted, (state, action) => {
+    return {
+      ...state,
+      collection: deleteBook(state.collection, action.bookId),
+    };
   })
 );
 
 export function reducer(state: State | undefined, action: Action) {
   return booksReducer(state, action);
 }
+
+export const selectAll = (state: State) => state.collection;
+export const selectSelectedBookId = (state: State) => state.selectedBookId;
+
+export const selectActiveBook = createSelector(
+  selectAll,
+  selectSelectedBookId,
+  (books, selectedBookdId) => {
+    return books.find((book) => book.id === selectedBookdId);
+  }
+);
+
+export const selectEarningsTotal = createSelector(
+  selectAll,
+  calculateBooksGrossEarnings
+);
